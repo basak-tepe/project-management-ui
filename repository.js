c4425e137930d08f13a24f6668708f1989464b11
Basak Tepe <basaktepe2020@gmail.com>
2024-07-03 11:18:36 +0300

    added create/update methods for repository and tracker

--- repository.js
+++ repository.js
@@ -1,1 +1,191 @@
-const repository = {template: '<div>Repositories</div>'};
+const repository = {
+    template: `
+<div>
+    <!-- Button to add new repository -->
+    <button 
+        type="button" class="btn btn-primary m-2 float-end"
+        data-bs-toggle="modal" 
+        data-bs-target="#exampleModal" 
+        @click="addClick()">
+        Add Repository
+    </button>
+
+    <!-- Table to display repositories -->
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Repository ID</th>
+                <th>Title</th>
+                <th>URL</th>
+                <th>Type</th>
+                <th>Email</th>
+                <th>Token</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="repo in repositories" :key="repo.repositoryID">
+                <td>{{ repo.repositoryID }}</td>
+                <td>{{ repo.title }}</td>
+                <td><a :href="repo.URL" target="_blank">{{ repo.URL }}</a></td>
+                <td>{{ repo.type }}</td>
+                <td>{{ repo.email }}</td>
+                <td>{{ repo.token }}</td>
+                <td>
+                    <button type="button" class="btn btn-light mr-1"
+                        data-bs-toggle="modal" 
+                        data-bs-target="#exampleModal" 
+                        @click="editClick(repo)">
+                        Edit
+                    </button>
+                    <button type="button" class="btn btn-light"
+                        @click="deleteClick(repo.repositoryID)">
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <!-- Modal for adding/editing repositories -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">{{ modalTitle }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="repoTitle" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="repoTitle" v-model="repoTitle">
+                    </div>
+                    <div class="mb-3">
+                        <label for="repoURL" class="form-label">URL</label>
+                        <input type="text" class="form-control" id="repoURL" v-model="repoURL">
+                    </div>
+                    <div class="mb-3">
+                        <label for="repoType" class="form-label">Type</label>
+                        <input type="text" class="form-control" id="repoType" v-model="repoType">
+                    </div>
+                    <div class="mb-3">
+                        <label for="repoEmail" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="repoEmail" v-model="repoEmail">
+                    </div>
+                    <div class="mb-3">
+                        <label for="repoToken" class="form-label">Token</label>
+                        <input type="text" class="form-control" id="repoToken" v-model="repoToken">
+                    </div>
+                    <button type="button" class="btn btn-primary" @click="repoId === 0 ? createRepository() : updateRepository()">
+                        {{ repoId === 0 ? 'Create' : 'Update' }}
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    `,
+    data() {
+        return {
+            repositories: [],
+            modalTitle: '',
+            repoTitle: '',
+            repoURL: '',
+            repoType: '',
+            repoEmail: '',
+            repoToken: '',
+            repoId: 0
+        };
+    },
+    methods: {
+        getRepositories() {
+            axios.get(variables.API_URL + 'repository')
+                .then(response => {
+                    this.repositories = response.data;
+                })
+                .catch(error => {
+                    console.error("There was an error fetching the repositories:", error);
+                });
+        },
+        addClick() {
+            this.modalTitle = 'Add Repository';
+            this.resetForm();
+        },
+        editClick(repo) {
+            this.modalTitle = 'Edit Repository';
+            this.repoId = repo.repositoryID;
+            this.repoTitle = repo.title;
+            this.repoURL = repo.URL;
+            this.repoType = repo.type;
+            this.repoEmail = repo.email;
+            this.repoToken = repo.token;
+        },
+        createRepository() {
+            // Prepare data to send to the backend
+            const data = {
+                title: this.repoTitle,
+                URL: this.repoURL,
+                type: this.repoType,
+                email: this.repoEmail,
+                token: this.repoToken
+            };
+
+            // Send POST request to create new repository
+            axios.post(variables.API_URL + 'repository/', data)
+                .then(response => {
+                    // Handle success, refresh repositories list
+                    this.getRepositories();
+                    this.resetForm();
+                    $('#exampleModal').modal('hide'); // Close modal
+                })
+                .catch(error => {
+                    console.error("Error creating repository:", error);
+                });
+        },
+        updateRepository() {
+            // Prepare data to send to the backend
+            const data = {
+                title: this.repoTitle,
+                URL: this.repoURL,
+                type: this.repoType,
+                email: this.repoEmail,
+                token: this.repoToken
+            };
+
+            // Send PUT request to update repository
+            axios.put(variables.API_URL + 'repository/' + this.repoId + '/', data)
+                .then(response => {
+                    // Handle success, refresh repositories list
+                    this.getRepositories();
+                    this.resetForm();
+                    $('#exampleModal').modal('hide'); // Close modal
+                })
+                .catch(error => {
+                    console.error("Error updating repository:", error);
+                });
+        },
+        deleteClick(repoId) {
+            // Send DELETE request to delete repository
+            axios.delete(variables.API_URL + 'repository/' + repoId + '/')
+                .then(response => {
+                    // Handle success, refresh repositories list
+                    this.getRepositories();
+                })
+                .catch(error => {
+                    console.error("Error deleting repository:", error);
+                });
+        },
+        resetForm() {
+            // Reset form fields
+            this.repoTitle = '';
+            this.repoURL = '';
+            this.repoType = '';
+            this.repoEmail = '';
+            this.repoToken = '';
+            this.repoId = 0;
+        }
+    },
+    mounted() {
+        this.getRepositories();
+    }
+};
--- tracker.js
+++ tracker.js
@@ -1,1 +1,191 @@
-const tracker = {template: '<div>Trackers</div>'};
+const tracker = {
+    template: `
+<div>
+    <!-- Button to add new tracker -->
+    <button 
+        type="button" class="btn btn-primary m-2 float-end"
+        data-bs-toggle="modal" 
+        data-bs-target="#exampleModal" 
+        @click="addClick()">
+        Add Tracker
+    </button>
+
+    <!-- Table to display trackers -->
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Tracker ID</th>
+                <th>Title</th>
+                <th>URL</th>
+                <th>Type</th>
+                <th>Email</th>
+                <th>Token</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="tracker in trackers" :key="tracker.TrackerID">
+                <td>{{ tracker.TrackerID }}</td>
+                <td>{{ tracker.title }}</td>
+                <td><a :href="tracker.URL" target="_blank">{{ tracker.URL }}</a></td>
+                <td>{{ tracker.type }}</td>
+                <td>{{ tracker.email }}</td>
+                <td>{{ tracker.token }}</td>
+                <td>
+                    <button type="button" class="btn btn-light mr-1"
+                        data-bs-toggle="modal" 
+                        data-bs-target="#exampleModal" 
+                        @click="editClick(tracker)">
+                        Edit
+                    </button>
+                    <button type="button" class="btn btn-light"
+                        @click="deleteClick(tracker.TrackerID)">
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <!-- Modal for adding/editing trackers -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">{{ modalTitle }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="trackerTitle" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="trackerTitle" v-model="trackerTitle">
+                    </div>
+                    <div class="mb-3">
+                        <label for="trackerURL" class="form-label">URL</label>
+                        <input type="text" class="form-control" id="trackerURL" v-model="trackerURL">
+                    </div>
+                    <div class="mb-3">
+                        <label for="trackerType" class="form-label">Type</label>
+                        <input type="text" class="form-control" id="trackerType" v-model="trackerType">
+                    </div>
+                    <div class="mb-3">
+                        <label for="trackerEmail" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="trackerEmail" v-model="trackerEmail">
+                    </div>
+                    <div class="mb-3">
+                        <label for="trackerToken" class="form-label">Token</label>
+                        <input type="text" class="form-control" id="trackerToken" v-model="trackerToken">
+                    </div>
+                    <button type="button" class="btn btn-primary" @click="trackerId === 0 ? createTracker() : updateTracker()">
+                        {{ trackerId === 0 ? 'Create' : 'Update' }}
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    `,
+    data() {
+        return {
+            trackers: [],
+            modalTitle: '',
+            trackerTitle: '',
+            trackerURL: '',
+            trackerType: '',
+            trackerEmail: '',
+            trackerToken: '',
+            trackerId: 0
+        };
+    },
+    methods: {
+        getTrackers() {
+            axios.get(variables.API_URL + 'tracker')
+                .then(response => {
+                    this.trackers = response.data;
+                })
+                .catch(error => {
+                    console.error("There was an error fetching the trackers:", error);
+                });
+        },
+        addClick() {
+            this.modalTitle = 'Add Tracker';
+            this.resetForm();
+        },
+        editClick(tracker) {
+            this.modalTitle = 'Edit Tracker';
+            this.trackerId = tracker.TrackerID;
+            this.trackerTitle = tracker.title;
+            this.trackerURL = tracker.URL;
+            this.trackerType = tracker.type;
+            this.trackerEmail = tracker.email;
+            this.trackerToken = tracker.token;
+        },
+        createTracker() {
+            // Prepare data to send to the backend
+            const data = {
+                title: this.trackerTitle,
+                URL: this.trackerURL,
+                type: this.trackerType,
+                email: this.trackerEmail,
+                token: this.trackerToken
+            };
+
+            // Send POST request to create new tracker
+            axios.post(variables.API_URL + 'tracker/', data)
+                .then(response => {
+                    // Handle success, refresh trackers list
+                    this.getTrackers();
+                    this.resetForm();
+                    $('#exampleModal').modal('hide'); // Close modal
+                })
+                .catch(error => {
+                    console.error("Error creating tracker:", error);
+                });
+        },
+        updateTracker() {
+            // Prepare data to send to the backend
+            const data = {
+                title: this.trackerTitle,
+                URL: this.trackerURL,
+                type: this.trackerType,
+                email: this.trackerEmail,
+                token: this.trackerToken
+            };
+
+            // Send PUT request to update tracker
+            axios.put(variables.API_URL + 'tracker/' + this.trackerId + '/', data)
+                .then(response => {
+                    // Handle success, refresh trackers list
+                    this.getTrackers();
+                    this.resetForm();
+                    $('#exampleModal').modal('hide'); // Close modal
+                })
+                .catch(error => {
+                    console.error("Error updating tracker:", error);
+                });
+        },
+        deleteClick(trackerId) {
+            // Send DELETE request to delete tracker
+            axios.delete(variables.API_URL + 'tracker/' + trackerId + '/')
+                .then(response => {
+                    // Handle success, refresh trackers list
+                    this.getTrackers();
+                })
+                .catch(error => {
+                    console.error("Error deleting tracker:", error);
+                });
+        },
+        resetForm() {
+            // Reset form fields
+            this.trackerTitle = '';
+            this.trackerURL = '';
+            this.trackerType = '';
+            this.trackerEmail = '';
+            this.trackerToken = '';
+            this.trackerId = 0;
+        }
+    },
+    mounted() {
+        this.getTrackers();
+    }
+};
